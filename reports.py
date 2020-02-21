59a16a49c14225af5d41f57bdbb091c5f6a7fc6a
Alexander Munz <a_munz@list.ru>
2020-02-21 12:06:54 +0300

    Refactoring

--- reports.py
+++ reports.py
@@ -1,14 +1,28 @@
+import logging
 from datetime import datetime
 import os
 from functools import reduce
 
 import requests
 
-users = requests.get('https://json.medrating.org/users').json()
-tasks = requests.get('https://json.medrating.org/todos').json()
+logging.basicConfig(level=logging.INFO)
 
 
-def stringify(user_id):
+def get_data(url):
+    try:
+        data = requests.get(url)
+        if data.status_code != 200:
+            logging.error(f'Ошибка! {url} Код ответа: {data.status_code}')
+            exit(1)
+        else:
+            logging.info(f'{url} Данные успешно получены.')
+    except Exception as e:
+        logging.exception(f'Ошибка! {e}')
+        exit(1)
+    return data.json()
+
+
+def stringify(user_id, tasks):
     user_tasks = filter(lambda task: task['userId'] == user_id, tasks)
 
     def helper(acc, task):
@@ -27,26 +41,36 @@ def cut(string):
     return string + '\n'
 
 
-if not os.path.exists('tasks'):
-    os.mkdir('tasks')
+def main():
+    users = get_data('https://json.medrating.org/users')
+    tasks = get_data('https://json.medrating.org/todos')
 
-for user in users:
-    date = datetime.now()
-    [complete, incomplete] = stringify(user['id'])
+    if not os.path.exists('tasks'):
+        os.mkdir('tasks')
 
-    text = f'''{user["name"]} <{user["email"]}> {date}
+    for user in users:
+        date = datetime.now().strftime('%d.%m.%Y %H:%M')
+        [complete, incomplete] = stringify(user['id'], tasks)
+
+        text = f'''
+{user["name"]} <{user["email"]}> {date}
 {user["company"]["name"]}
 
 Завершенные задачи:
-{complete}
+{complete.strip()}
 
 Оставшиеся задачи:
-{incomplete}
+{incomplete.strip()}
 '''
-    filename = f'tasks/{user["username"]}.txt'
-    if os.path.exists(filename):
-        with open(f'tasks/{user["username"]}_{date.strftime("%Y-%m-%dT%T")}.txt', 'w', encoding='utf8') as f:
-            f.write(text)
-    else:
+        filename = f'tasks/{user["username"]}.txt'
+        if os.path.exists(filename):
+            mod_date = datetime.fromtimestamp(os.path.getmtime(filename)).strftime("%Y-%m-%dT%H:%M")
+            os.renames(filename, f'{filename[:-4]}_{mod_date}.txt')
         with open(f'tasks/{user["username"]}.txt', 'w', encoding='utf8') as f:
-            f.write(text)
+            f.write(text.strip())
+
+    logging.info('OK. Отчеты успешно сформированы.')
+
+
+if __name__ == '__main__':
+    main()
